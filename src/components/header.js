d3dce24eff00b04b3c953da8ccaba231a1e53414
GitJay37 <jeisondejesus@hotmail.es>
2020-04-07 15:33:57 -0500

    styles ok

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import images from '../images/tabs.svg';
+import Form from '../components/contact.form';
+
 export default () => (
-    <header className="bg-blue-300">
-        <div className="container mx-auto p-12 max-w-4xl flex justify-center items-center">
-            <div className="flex-1">
-                <h1 className="font-bold text-blue-700 text-6xl"> Hola I'm Jason </h1>
-                <p className="text-xl font-light"> Soy ingeniero informatico en aprendizaje constante</p>
+    <header className="bg-gray-300">
+        <div className="container mx-auto p-12 max-w-4xl">
+
+            <div className="flex justify-center items-center">
+                <div className="flex-1">
+                    <h1 className="font-bold text-blue-700 text-6xl"> Hola I'm Jason </h1>
+                    <p className="text-xl font-light"> Soy ingeniero informatico en aprendizaje constante</p>
+                </div>
+                <img src={images} alt="Hombre manipulando pestañas web" style={{height: "200px"}}></img>
             </div>
-            <img src={images} alt="Hombre manipulando pestañas web" style={{height: "200px"}}></img>
+            
+            <div>
+                <Form/>
+            </div>
         </div>
     </header>
 )
