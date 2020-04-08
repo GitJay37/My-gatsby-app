0994a26ab9f4afb26caf095013e87c748bc556c6
GitJay37 <jeisondejesus@hotmail.es>
2020-04-07 22:00:32 -0500

    uploading to github

--- src/components/contact.form.js
+++ src/components/contact.form.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default () => (
     <form className="mt-16 text-center">
-        <label for="contact-content" className="block text-blue-700 text-sm font-bold mb-2"> Dime acerca de las ideas que quieres hacer realidad!! </label>
+        <label htmlFor="contact-content" className="block text-blue-700 text-sm font-bold mb-2"> Dime acerca de las ideas que quieres hacer realidad!! </label>
         <div className="flex shadow rounded bg-white border p-2">
             <textarea 
                 id="contact-content"
@@ -10,7 +10,7 @@ export default () => (
                 className="flex-1 py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline">
 
             </textarea>
-            <button class="btn ml-4">Enviar</button>
+            <button className="btn ml-4">Enviar</button>
         </div>
     </form>    
 );
